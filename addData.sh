b893acf5ef80c59b0f5f8ad369332081e4d3af55
kylejamesbyron <markandrewisom@gmail.com>
2023-04-02 05:46:15 -0400

    Started easy add script for logging films

--- addData.sh
+++ addData.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "Name:"
+read name
+echo "Film:"
+read film
+echo "Logline:"
+read logline
 
 echo "Testing sqlite"
 
-sqlite3 twl.db "INSERT INTO Screenplays Values('Tom', 'Johns Movie', 'A Story');"
+sqlite3 data.db "INSERT INTO Screenplays Values('$name', '$film', '$logline');"
 
-sqlite3 twl.db "select * from Screenplays;
+sqlite3 data.db "select * from Screenplays;
 echo "Done"
