2f937571d31ba57f8d5833ef5fd2e900662875cf
kylejamesbyron <markandrewisom@gmail.com>
2023-04-03 13:49:23 -0400

    More work on Database

--- mark.py
+++ mark.py
@@ -8,7 +8,7 @@ cursor = connection.cursor()
 #cursor.execute("CREATE TABLE media (title TEXT, author TEXT, media TEXT, description TEXT)") 
 
 # To Insert Values
-cursor.execute("INSERT INTO media VALUES ('Linux Reality Podcast', 'Chess Griffin', 'Instructional', 'Apodcast for the new linux user')")
+#cursor.execute("INSERT INTO media VALUES ('Linux Reality Podcast', 'Chess Griffin', 'Instructional', 'Apodcast for the new linux user')")
 
 #To read values
 rows = cursor.execute("SELECT title, author, media, description FROM media WHERE title = ?", ('Linux Reality Podcast',),).fetchall()
