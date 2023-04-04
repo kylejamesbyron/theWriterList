afbff9ed13c1ffb5467e753b0a046ea3694e97df
kylejamesbyron <markandrewisom@gmail.com>
2023-04-04 09:51:57 -0400

    added clear function

--- mediaManager.py
+++ mediaManager.py
@@ -8,6 +8,9 @@ import sqlite3
 connection = sqlite3.connect("mediamanager.db")
 cursor = connection.cursor()
 
+#import linux os calls
+import os
+
 print("Welcome to Media Manager")
 print("1. Insert Table")
 print("2. Insert Full Values")
@@ -16,34 +19,39 @@ print("4. Perform Query")
 print("5. Show Table")
 choice = input("Choice: ")
 
-if choice == "1"
-    # To insert table
-    input("Table Name: ")
+if choice == "1":
+# To insert table
+    tableName = input("Table Name: ")
     #input headers as list
-    #cursor.execute("CREATE TABLE media (title TEXT, author TEXT, media TEXT, description TEXT)") 
-elif
-    choice == "2"
-    # To add values
+    cursor.execute("CREATE TABLE media (title TEXT, author TEXT, media TEXT, description TEXT)") 
+
+elif choice == "2":
+# To add values
     title = input("title:")
     author = input("author:")
     media = input("media:")
     description = input("description:")
-    print("Record:")
+    os.system('clear')
+    print("RECORD TO REVIEW:")
+    print('Title:')
     print(title)
+    print('Author:')
     print(author)
+    print('Media:')
     print(media)
+    print('Description:')
     print(description)
-    okay = input("Type OK to commit")
-    # To commit changes
-        if okay == "OK"
-        # To Insert Values
-            cursor.execute("INSERT INTO media (title, author, media, description) values (?, ?, ?, ?)", 
-		        (title, author, media, description))
-            #commit connection:
-            connection.commit()
-        else
-            print("Not commited")
-else
+    okay = input("Type OK to commit: ")
+ #  To commit changes
+    if okay == "OK":
+# To Insert Values
+        cursor.execute("INSERT INTO media (title, author, media, description) values (?, ?, ?, ?)", 
+		  (title, author, media, description))
+#commit connection:
+        connection.commit()
+    else:
+        print("Not commited")
+else:
     print("All done")
 
 #To read values
@@ -51,8 +59,8 @@ else
 #print(rows)
 
 # Show full table
-connection.execute("SELECT * FROM media")
-print(connection.fetchall)
+cursor.execute("SELECT * FROM media")
+print(cursor.fetchall)
 
 
 print("Total Changes:  ")
