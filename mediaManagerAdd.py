3d7c46c47484d2608288e7dca1246e605de7d9aa
kylejamesbyron <markandrewisom@gmail.com>
2023-04-04 04:57:05 -0400

    A simple working version of the add function for media manager

--- mediaManagerAdd.py
+++ mediaManagerAdd.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("mark.db")
+connection = sqlite3.connect("tester.db")
 
 cursor = connection.cursor()
 
@@ -8,14 +8,21 @@ cursor = connection.cursor()
 #cursor.execute("CREATE TABLE media (title TEXT, author TEXT, media TEXT, description TEXT)") 
 
 title = input("title:")
-input("author:")
-input("media:")
-input("description:")
+author = input("author:")
+media = input("media:")
+description = input("description:")
 
+print("Variables:")
 print(title)
+print(author)
+print(media)
+print(description)
+
 
 # To Insert Values
-cursor.execute("INSERT INTO media VALUES (title, author, media, description)")
+cursor.execute("INSERT INTO media (title, author, media, description) values (?, ?, ?, ?)", 
+		(title, author, media, description))
+connection.commit()
 
 #To read values
 rows = cursor.execute("SELECT title, author, media, description FROM media WHERE title = ?", ('Linux Reality Podcast',),).fetchall()
